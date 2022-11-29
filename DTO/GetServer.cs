ac599743c6c7b8dcf77145ff4113700006e87fac
Milena Elen <elenprojects7@gmail.com>
2022-11-29 14:09:44 -0300

    REFACTOR: add class GetServer

---
+++ DTO/GetServer.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace CovidReport.DTO
+{
+    public class GetServer
+    {
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("data")]
+        public Data Data { get; set; }
+    }
+
+    public class Data
+    {
+        [JsonProperty("server")]
+        public string Server { get; set; }
+    }
+
+}
--- Services/SendService.cs
+++ Services/SendService.cs
@@ -1,25 +1,8 @@
-﻿using Microsoft.AspNetCore.Components.Server;
-using Microsoft.AspNetCore.Hosting.Server;
+﻿using CovidReport.DTO;
 using Newtonsoft.Json;
-using System.Net;
 
 namespace CovidReport.Services
 {
-    public class GetServer
-    {
-        [JsonProperty("status")]
-        public string Status { get; set; }
-
-        [JsonProperty("data")]
-        public Data Data { get; set; }
-    }
-
-    public class Data
-    {
-        [JsonProperty("server")]
-        public string Server { get; set; }
-    }
-
     public class SendService
     {
         public async Task<string> GetBestServer()
@@ -66,7 +49,7 @@ namespace CovidReport.Services
             return requestContent;
 
         }
-        public async Task<int> SendFormData(IConfiguration _config, string path)
+        public async void SendFormData(IConfiguration _config, string path)
         {
 
             var requestContent = MakeContentRequest(_config, path);
@@ -77,7 +60,7 @@ namespace CovidReport.Services
 
             var response = await client.PostAsync(server, requestContent);
 
-            var responseString = await response.Content.ReadAsStringAsync();
+             // var responseString = await response.Content.ReadAsStringAsync();
 
             if (!response.IsSuccessStatusCode)
             {
@@ -90,14 +73,6 @@ namespace CovidReport.Services
             //}
 
             FileService.DeleteFile(path);
-
-            Console.WriteLine(server);
-
-            Console.WriteLine(response.StatusCode);
-
-            Console.WriteLine(responseString);
-
-            return 2;
         }
     }
 }
