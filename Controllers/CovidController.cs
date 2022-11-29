8f900a50419914d3a2e54ae68fc4a3e24fce75e7
Milena Elen <elenprojects7@gmail.com>
2022-11-29 09:12:15 -0300

    FEAT: create covidController

---
+++ Controllers/CovidController.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CovidReport.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CovidController : ControllerBase
+    {
+        private readonly ILogger<CovidController> _logger;
+
+        public CovidController(ILogger<CovidController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetCovidAll")]
+        public async Task<CovidAll> Get()
+        {
+            var url = "https://disease.sh/v3/covid-19/all";
+
+            var requisicao = new HttpClient();
+
+            var stringTask = await requisicao.GetAsync(url);
+
+            var jsonResponse = await stringTask.Content.ReadAsStringAsync();
+
+            CovidAll covid = JsonConvert.DeserializeObject<CovidAll>(jsonResponse);
+
+            return covid;
+
+        }
+    }
+}
