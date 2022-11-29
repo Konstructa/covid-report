99f14902b47569a25208e28db9f9be93cadffb3c
Milena Elen <elenprojects7@gmail.com>
2022-11-29 10:15:19 -0300

    FEAT: create fileservice class with create and delete file

---
+++ FileService.cs
@@ -0,0 +1,35 @@
+﻿namespace CovidReport
+{
+    public class FileService
+    {
+        public static string CreateFile(CovidAll covidAll)
+        {
+            var path = @"C:\temp";
+
+            Directory.CreateDirectory(path + @"\reports");
+
+            var fileName = $"report-{DateTime.Now.Date}.csv";
+
+            var targetPath = Path.GetDirectoryName(path) + @"\reports\" + fileName;
+
+            using (StreamWriter streamWriter = File.AppendText(targetPath))
+            {
+                streamWriter.WriteLine(covidAll.ToString());
+            }
+
+            return targetPath;
+        }
+
+        public static void DeleteFile(string path)
+        {
+            try
+            {
+                File.Delete(path);
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
